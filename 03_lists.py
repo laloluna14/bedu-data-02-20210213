f47d12d24d5688b0029da0c83c4b6e32fad9dd2c
Lalo Luna <57363601+laloluna14@users.noreply.github.com>
2021-02-13 10:39:19 -0600

    Exercises 1-4

---
+++ 03_lists.py
@@ -0,0 +1,11 @@
+numbers = [
+    23,
+    45,
+    90,
+    234,
+    23,
+    90
+]
+
+print(type(numbers))
+print(numbers)
---
+++ 04_lists_add_element.py
@@ -0,0 +1,14 @@
+initial_list = []
+# initial_list = list
+
+element_string = 'Bedu'
+element_int = 10
+element_double = 3.14
+element_dic = {1: 'hello', 2: 'world'}
+
+initial_list.append(element_string)
+initial_list.append(element_int)
+initial_list.append(element_double)
+initial_list.append(element_dic)
+
+print(initial_list)
